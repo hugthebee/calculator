0740c4184ee28464df2c9253c6323958fff6e342
sreeja <sreeja.guduri@gmail.com>
2023-06-18 22:48:52 +0400

    added decimal number functionality

--- script.js
+++ script.js
@@ -7,35 +7,65 @@ secondNum = "";
 screen.textContent = display;
 
 const ac = document.querySelector("#ac");
+ac.addEventListener("mouseover",()=>{
+    console.log("hi");
+    ac.classList.add("select");
+});
+ac.addEventListener("mouseleave",()=>{
+    ac.classList.remove("select");
+});
 ac.addEventListener("click",()=>
 {
     display = "";
     screen.textContent = display;
-
+    calc=0; firstNum=0; secondNum=0;
 });
+
+
 const ce = document.querySelector("#ce");
+ce.addEventListener("mouseover",()=>{
+    console.log("hi");
+    ce.classList.add("select");
+});
+
+ce.addEventListener("mouseleave",()=>{
+    ce.classList.remove("select");
+});
 ce.addEventListener("click",()=>
 {
+    display = display.toString();
     display = display.slice(0,-1);
-    if(!display) display = "0";
+    if(!display) {display = "0"; calc=0; firstNum=0; secondNum=0;}
     screen.textContent = display;
 });
 
 
 function add(a,b){
-    return a+b;
+    let ans = a+b;
+    if (Math.floor(ans)!=ans && ans.toString().split(".")[1].length >= 8)
+        return ans.toFixed(8)
+    return ans;
 }
 
 function sub(a,b){
-    return a-b;
+    let ans = a-b;
+    if (Math.floor(ans)!=ans && ans.toString().split(".")[1].length >= 8)
+        return ans.toFixed(8)
+    return ans;
 }
 
 function mul(a,b){
-    return (a*b);
+    let ans = a*b;
+    if (Math.floor(ans)!=ans && ans.toString().split(".")[1].length >= 8)
+        return ans.toFixed(8)
+    return ans;
 }
 
 function divide(a,b){
-    return (a/b);
+    let ans = a/b;
+    if (Math.floor(ans)!=ans && ans.toString().split(".")[1].length >= 8)
+        return ans.toFixed(8)
+    return ans;
 }
 
 function operate(first,second,op){
@@ -53,7 +83,7 @@ nums = Array.from(nums);
 for(let i=0;i<nums.length;i++){
     nums[i].addEventListener("click",()=>
     {
-        if(nums[i].textContent==="=") calculate(display);
+        if(nums[i].textContent==="=") {calc=0; calculate(display);}
         else if(nums[i].textContent === "+"||nums[i].textContent==="-"||nums[i].textContent==="*"||nums[i].textContent==="/")
         {
             display= display+" "+ nums[i].textContent + " ";
@@ -65,11 +95,20 @@ for(let i=0;i<nums.length;i++){
         if(display[0]==="0" && calc===0) display=display.slice(1); 
         screen.textContent = display;
     });
+
+    nums[i].addEventListener("mouseover",()=>{
+        nums[i].classList.add("select");
+    });
+
+    nums[i].addEventListener("mouseleave",()=>{
+        nums[i].classList.remove("select");
+    });
 }
 
 function calculate(s)
 {
     let arr = s.split(" ");
+    if(!arr[2]) return;
     firstNum = arr[0];
     op = arr[1];
     secondNum = arr[2];
